4bde54020312e476cf708ef238650c3928fde630
Алексей <alexsynarchin@gmail.com>
2021-12-31 11:13:37 +0500

    Comments

--- resources/site/js/components/comment/index.vue
+++ resources/site/js/components/comment/index.vue
@@ -4,9 +4,38 @@
             <h3 class="comments__title">
                 Комментарии к курсу :
             </h3>
-            <button class="btn comments__button">
+            <button class="btn comments__button" @click.prevent="addComment">
                 Добавить комментарий
             </button>
         </div>
     </section>
 </template>
+<script>
+    export default {
+        methods: {
+            addComment() {
+                if(this.authCheck) {
+
+                } else {
+                    this.$modal.show('isLogin');
+                }
+            },
+        },
+        computed: {
+            signedIn() {
+                return window.App.signedIn;
+            },
+            user(){
+                return  window.App.user;
+            },
+            authCheck() {
+                if(this.signedIn) {
+                    return (this.user.profile_type === 'teacher') || (this.user.profile_type === 'student')
+                } else {
+                    return false;
+                }
+
+            }
+        },
+    }
+</script>
