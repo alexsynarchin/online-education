6ddd89c1955ecb5e90cd7bec6aeb1e6a286b18d8
Алексей <alexsynarchin@gmail.com>
2022-01-06 15:00:47 +0500

    admin teachers page

--- resources/admin/js/router.js
+++ resources/admin/js/router.js
@@ -2,14 +2,16 @@ import Vue from "vue";
 import VueRouter from 'vue-router';
 import AdminHome from './views/home/index';
 import CourseItem from './views/home/show';
-import AdminStudents from './views/students/index';
+import AdminStudents from './views/student/index';
 import CategoryType from './views/categories/index';
+import AdminTeachers from './views/teacher/index'
 Vue.use(VueRouter);
 let routes = [
     { name: 'home', path: '/admin', component: AdminHome},
     { name: 'course-item', path: '/admin/courses/:id', component: CourseItem, props: true},
     { name: 'category-types', path: '/admin/categories', component: CategoryType},
     { name: 'students', path: '/admin/students', component: AdminStudents},
+    { name: 'teachers', path: '/admin/teachers', component: AdminTeachers},
 ];
 export default new VueRouter({
     mode: 'history',
---
+++ resources/admin/js/views/teacher/index.vue
@@ -0,0 +1,94 @@
+<template>
+    <section class="page-content">
+        <el-row type="flex" class="mb-3">
+            <el-col :span="8">
+                <el-input
+                    v-model="filters[0].value"
+                    size="large"
+                    width="100%"
+                    placeholder="Поиск: Имя, Фамилия или Отчество"/>
+            </el-col>
+        </el-row>
+        <data-tables :data="teachers"  :filters="filters">
+            <el-table-column
+                type="index"
+            >
+            </el-table-column>
+            <el-table-column
+                label="Фамилия"
+                prop="surname"
+                sortable
+            >
+            </el-table-column>
+            <el-table-column
+                label="Имя"
+                prop="name"
+                sortable
+            >
+            </el-table-column>
+            <el-table-column
+                label="Отчество"
+                prop="last_name"
+                sortable
+            >
+            </el-table-column>
+            <el-table-column
+                label="E-mail"
+                prop="email"
+                sortable
+            >
+            </el-table-column>
+            <el-table-column
+                label="Дата регистрации"
+                prop="formatted_created_at"
+                sortable
+            >
+            </el-table-column>
+            <el-table-column
+                label="Действия"
+            >
+
+                <template slot-scope="scope">
+                    <el-button
+                        size="mini"
+                        type="primary"
+                        @click="handleEdit(scope.$index, scope.row)">Просмотр</el-button>
+
+                </template>
+            </el-table-column>
+        </data-tables>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            teachers:[],
+            filters: [
+                {
+                    prop: ['name', 'surname', 'last_name'],
+                    value: ""
+                }
+            ],
+        }
+    },
+    mounted() {
+        this.getTeachers();
+    },
+    methods: {
+        handleEdit(index, row) {
+            var url = '/admin/teachers/' + row.id;
+            window.location = url;
+        },
+        getTeachers() {
+            axios.get('/api/admin/users', {params: {type:'teacher'}})
+                .then((response) => {
+                    this.teachers= response.data;
+                })
+                .catch((error) => {
+
+                })
+        }
+    }
+}
+</script>
