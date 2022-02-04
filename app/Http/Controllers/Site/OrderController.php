c3dc42bf80d5378dd667e7f44be29a4242de0040
Алексей <alexsynarchin@gmail.com>
2022-02-04 15:40:28 +0500

    footer menu

--- app/Http/Controllers/Site/OrderController.php
+++ app/Http/Controllers/Site/OrderController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account\StudentAccount;
+use App\Models\Category\Course;
+use App\Models\Lesson\Lesson;
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -11,11 +15,47 @@ class OrderController extends Controller
     public function success(Request $request)
     {
         $order = Order::findOrFail($request->get('InvId'));
-        dd($order);
+        if($order->type === 'course') {
+            $course = Course::findOrFail($order->buying_id);
+            $lessons = $course->lessons()->get();
+            foreach ($lessons as $lesson) {
+                $this->lessonToStudent($lesson, $order->student_id);
+            }
+            $url = route('catalog.show', [
+                'edu_slug' => $course -> edu_type -> slug,
+                'slug' => $course->slug
+            ]);
+            $teacher = User::findOrFail($course->author_id);
+        } else {
+            $lesson = Lesson::findOrFail($order->buying_id);
+            $this->lessonToStudent($lesson, $order->student_id);
+            $teacher = User::findOrFail($lesson->user_id);
+            $course = Course::findOrFail($lesson->course_id);
+            $url = route('lesson.show', [
+                'edu_slug' => $course -> edu_type -> slug,
+                'course_slug' => $course->slug,
+                'slug' => $lesson->slug
+            ]);
+        }
+        $teacher = $teacher -> teacherAccount;
+        $teacher->balance = $teacher->balance + $request->get('price');
+        $teacher->save();
+        $teacher_id = $teacher -> id;
+        $student = StudentAccount::findOrFail($order->student_id);
+        if(!$student->teachers()->where('student_id', $student->id)->exists()) {
+            $student->teachers()->attach($teacher_id);
+        }
+        redirect($url) ;
     }
 
     public function fail(Request $request)
     {
         dd($request->all());
+    }
+    private function lessonToStudent($lesson, $student_id)
+    {
+        if(!$lesson->students()->where('student_id', $student_id) -> exists()){
+            $lesson->students()->attach($student_id);
+        }
     }
 }
