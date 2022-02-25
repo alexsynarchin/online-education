953ee52f0e16ae4bfc3893d9852841ec2b600b6b
Алексей <alexsynarchin@gmail.com>
2022-02-25 15:30:43 +0500

    lesson test

--- resources/site/js/components/lessont-test/index.vue
+++ resources/site/js/components/lessont-test/index.vue
@@ -1,5 +1,6 @@
 <template>
     <section class="lesson-test" v-if="loaded && test">
+
         <h4 class="lesson-test__title">
             Тест к уроку
         </h4>
@@ -23,17 +24,26 @@
             :title="'Тест к уроку: ' + lesson_title"
         >
         <form action="" class="test-form">
-            <fieldset class="test-form-item" v-for="(question, question_index) in test.questions">
-                <div class="test-header">
-                    <span class="test-header__count">{{question_index + 1}}</span>
-                    <div class="test-header__title" v-html="question.text"></div>
-                </div>
-                <div class="test-image-wrap" v-if="question.preview">
-                    <img :src="question.preview"/>
-                </div>
+            <div class="test-header">
+                    <span class="test-header__count">
+                        Вопрос {{question_index + 1}}
+                    </span>
+                    <div class="test-header__right">
+                        <span class="test-header__current-num">
+                            Пройдено: {{question_index}} из {{test.questions.length}}
+                        </span>
+                    </div>
+            </div>
+            <fieldset class="test-form-item" :class="{'test-form-item--current': index === question_index}" v-for="(question, index) in test.questions" >
+                <section class="test-question">
+                    <div class="test-question__text" v-html="question.text"></div>
+                    <div class="test-question__image" v-if="question.preview">
+                        <img :src="question.preview"/>
+                    </div>
+                </section>
+
                 <div class="test-body">
                     <div class="test-body-item" v-for="(option, option_index) in question.options">
-                        <span class="test-body-item__number">{{option_index+1}})</span>
                         <input type="checkbox" v-model="option.answer"  class="test-body-item__checkbox">
                         <div class="test-body-item__content d-flex">
                             <div class="test-option__image-wrap" v-if="option.preview">
@@ -41,12 +51,10 @@
                             </div>
                             <div class="test-body-item__questation" v-html="option.text"></div>
                         </div>
-
-
                     </div>
                 </div>
             </fieldset>
-            <button class="test-form-btn" type="submit" @click.prevent="testPass(test)">Отправить</button>
+            <button class="test-form-btn" type="submit" @click.prevent="testPass(test)">Ответить</button>
         </form>
         </el-dialog>
         <el-dialog
@@ -81,12 +89,13 @@
         },
         data() {
             return {
+                question_index: 0,
                 loaded:false,
                 test: {},
                 result:{},
                 answers:[],
                 can_test:true,
-                dialogVisible:false,
+                dialogVisible:true,
                 resultVisible:false,
             }
         },
@@ -110,27 +119,32 @@
                 })
             },
             testPass(test){
-                var results = [];
-                for (var i = 0; i < test.questions.length; i++){
-                    results[i] = {question_id: test.questions[i].id, answers: []};
-                    for (var k = 0; k < test.questions[i].options.length; k++){
-                        results[i].answers[k] = {
-                            option_id: test.questions[i].options[k].id,
-                            answer:test.questions[i].options[k].answer,
-                            right_answer: test.questions[i].options[k].right_answer,
+                this.question_index++;
+                if((this.question_index) === test.questions.length) {
+                    this.question_index = 0;
+                    var results = [];
+                    for (var i = 0; i < test.questions.length; i++){
+                        results[i] = {question_id: test.questions[i].id, answers: []};
+                        for (var k = 0; k < test.questions[i].options.length; k++){
+                            results[i].answers[k] = {
+                                option_id: test.questions[i].options[k].id,
+                                answer:test.questions[i].options[k].answer,
+                                right_answer: test.questions[i].options[k].right_answer,
+                            }
                         }
                     }
+                    axios.post('/api/education/test/' + test.id + '/pass', {results:results})
+                        .then((response)=>{
+                            this.dialogVisible = false;
+                            this.getResult();
+                            this.getTests();
+                            this.resultVisible = true;
+                        })
+                        .catch((error)=>{
+                            console.log(error)
+                        })
                 }
-                axios.post('/api/education/test/' + test.id + '/pass', {results:results})
-                    .then((response)=>{
-                        this.dialogVisible = false;
-                        this.getResult();
-                        this.getTests();
-                        this.resultVisible = true;
-                    })
-                    .catch((error)=>{
-                        console.log(error)
-                    })
+
             }
         },
         mounted() {
