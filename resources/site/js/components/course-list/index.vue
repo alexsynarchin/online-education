e08edf950ea05c719fbe2a6b322093603e8fbde7
Алексей <alexsynarchin@gmail.com>
2022-03-02 14:37:16 +0500

    categories in course list in teacher profile

--- resources/site/js/components/course-list/index.vue
+++ resources/site/js/components/course-list/index.vue
@@ -1,5 +1,13 @@
 <template>
     <section class="course-list" v-if="(courses.length > 0) ">
+        <section class="course-list-sort">
+            <label class="course-list-sort__label">
+                Сортировать по:
+            </label>
+            <span class="course-list-sort__item" v-for="(item, index) in SortList">
+                {{item.title}}
+            </span>
+        </section>
         <CourseItem
             v-for="(course, index) in courses"
             :key="course.id"
@@ -27,6 +35,24 @@ import CourseItem from "./item";
             return {
                 courses: [],
                 loaded:false,
+                SortList: [
+                    {
+                        title:'Рейтингу',
+                        value:'rating',
+                        direction:'ASC',
+                    },
+                    {
+                        title:'Новизне',
+                        value:'created_at',
+                        direction:'ASC',
+                    },
+                    {
+                        title:'Цене',
+                        value:'price',
+                        direction:'ASC',
+                    },
+
+                ],
             }
         },
         methods: {
