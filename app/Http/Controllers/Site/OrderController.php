f0f3c5c252ce1252780631a9e1b8dcc48be499fb
Алексей <alexsynarchin@gmail.com>
2022-04-06 16:29:30 +0500

    Add promo_balance for teacher

--- app/Http/Controllers/Site/OrderController.php
+++ app/Http/Controllers/Site/OrderController.php
@@ -19,7 +19,7 @@ class OrderController extends Controller
         $order = Order::findOrFail($request->get('InvId'));
         if($order->type === 'course') {
             $course = Course::findOrFail($order->buying_id);
-            $price = $course->price;
+            $price = $course-> lessons -> sum('price_user');;
             $lessons = $course->lessons()->get();
             foreach ($lessons as $lesson) {
                 $this->lessonToStudent($lesson, $order->student_id);
