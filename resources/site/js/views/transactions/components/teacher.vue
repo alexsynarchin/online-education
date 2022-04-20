fcf1e610658189ef77bee88f0543b018a786af03
Алексей <alexsynarchin@gmail.com>
2022-04-20 22:06:43 +0500

    fix bugs

--- resources/site/js/views/transactions/components/teacher.vue
+++ resources/site/js/views/transactions/components/teacher.vue
@@ -15,8 +15,15 @@
             </div>
             <div class="b-table-transac__body">
                 <div class="b-table-transac__item" v-for="(item, index) in withdraws" :key="index">
-                    <div class="b-table-transac__item_count">-{{item.sum}} руб. </div>
-                    <div class="b-table-transac__item_des">Вывод средств</div>
+                    <div class="b-table-transac__item_count"
+                        :class="{
+                        'b-table-transac__item_count--danger':!item.done
+                        }"
+                    >-{{item.sum}} руб. </div>
+                    <div class="b-table-transac__item_des">
+                        Вывод средств
+                        (<template v-if="item.done">Одобрено</template><template v-else>Ждет одобрения</template>)
+                    </div>
                     <div class="b-table-transac__item_date">{{item.formatted_created_at}}</div>
                 </div>
             </div>
