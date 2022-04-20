039f0b281eef7415afc91a102536527682e56c11
Алексей <alexsynarchin@gmail.com>
2022-04-20 21:55:40 +0500

    fix bugs

--- app/Http/Controllers/Api/Profile/WithdrawController.php
+++ app/Http/Controllers/Api/Profile/WithdrawController.php
@@ -12,12 +12,20 @@ class WithdrawController extends Controller
     public function withdraw(Request $request)
     {
         $user = User::findOrFail($request->get('user_id'));
+        $withdraw_sum = $request->get('withdraw_sum');
         $teacher_account= $user->teacherAccount;
         $teacher_account->card_number = $request->get('card_number');
+        if($teacher_account->balance < $withdraw_sum) {
+            $withdraw_sum = $withdraw_sum - $teacher_account->balance;
+            $teacher_account->balance = 0;
+            $teacher_account->promo_balance = $teacher_account->promo_balance - $withdraw_sum;
+        } else {
+            $teacher_account->balance = $teacher_account->balance - $withdraw_sum;
+        }
         $teacher_account->save();
         $withdraw = Withdraw::create([
             'teacher_id' => $user->id,
-            'sum' => $request->get('sum')
+            'sum' => $request->get('withdraw_sum')
         ]);
         return $request->all();
     }
