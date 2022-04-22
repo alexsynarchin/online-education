5a66dc29eb0d65525f512f2c244071e2f9646068
Алексей <alexsynarchin@gmail.com>
2022-04-22 10:49:54 +0500

    fix bugs

--- resources/common/js/components/richtext/index.vue
+++ resources/common/js/components/richtext/index.vue
@@ -10,7 +10,7 @@
            'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
          ],
          toolbar:
-           'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl'
+           'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor  removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl'
        }"
     />
 </template>
