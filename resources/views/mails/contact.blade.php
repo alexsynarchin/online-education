5a5914eb06b2a8a1e32959113cd0bc7f7c0e27ef
Алексей <alexsynarchin@gmail.com>
2022-05-08 23:30:50 +0500

    Regions

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -1,6 +1,6 @@
 <label>Тема сообщения:</label> <span>{{$contact['theme'] }}</span><br>
 <label>Имя:</label> <span>{{$contact['name'] }}</span><br>
-<label>E-mail:</label> <span>{{$contact -> email}}</span><br>
-<label>Тип пользователя:</label> <span>{{$contact -> type}}</span><br>
+<label>E-mail:</label> <span>{{$contact['email']}}</span><br>
+<label>Тип пользователя:</label> <span>{{$contact['type']}}</span><br>
 <label>Комментарий:</label>
-<p>{{$contact -> text}}</p> <br>
+<p>{{$contact['text']}}</p> <br>
