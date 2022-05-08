dcff227147997e4a709e7c4fb28835bd9bee3668
Алексей <alexsynarchin@gmail.com>
2022-05-08 23:24:35 +0500

    Regions

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -1,3 +1,6 @@
-<p>
-    Тестовое письмо
-</p>
+<label>Тема сообщения:</label> <span>{{$contact -> theme ? $contact -> tneme : 'none'}}</span><br>
+<label>Имя:</label> <span>{{$contact -> name ? $contact -> name : 'none'}}</span><br>
+<label>E-mail:</label> <span>{{$contact -> email}}</span><br>
+<label>Тип пользователя:</label> <span>{{$contact -> type}}</span><br>
+<label>Комментарий:</label>
+<p>{{$contact -> text}}</p> <br>
