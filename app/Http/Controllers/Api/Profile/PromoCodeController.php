37dc4a7578eeb8f92fe5caf159e7f2578c20bad6
Алексей <alexsynarchin@gmail.com>
2022-05-09 11:36:26 +0500

    Regions

--- app/Http/Controllers/Api/Profile/PromoCodeController.php
+++ app/Http/Controllers/Api/Profile/PromoCodeController.php
@@ -7,11 +7,13 @@ use App\Models\Account\StudentAccount;
 use App\Models\PromoCode;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Carbon\Carbon;
 
 class PromoCodeController extends Controller
 {
     public function handlePromoCode(Request $request)
     {
+
         $request->validate([
             'name'=>['required', 'exists:promo_codes',
                 function ($attribute, $value, $fail) use ($request) {
@@ -22,7 +24,27 @@ class PromoCodeController extends Controller
                 if($exists) {
                     $fail('Данный промокод уже использован');
                 }
-        }]],  [
+        },function ($attribute, $value, $fail) use ($request) {
+                   $promo_code  = PromoCode::where('name', $request->get('name')) ->first();
+
+                    if($promo_code->max_count < 1) {
+                        $fail('Данный промокод не может больше  использоваться');
+                    }
+                }, function ($attribute, $value, $fail) use ($request) {
+                    $promo_code  = PromoCode::where('name', $request->get('name')) ->first();
+                    $date1 = Carbon::parse(date('Y-m-d'));
+                    $date2 = Carbon::parse(date('Y-m-d', strtotime($promo_code->end_time)));
+                    if($date1< $date2) {
+                        $fail('Данный промокод не может больше  использоваться');
+                    }
+                }, function ($attribute, $value, $fail) use ($request) {
+                    $promo_code  = PromoCode::where('name', $request->get('name')) ->first();
+                    $date1 = Carbon::parse(date('Y-m-d'));
+                    $date2 = Carbon::parse(date('Y-m-d', strtotime($promo_code->start_time)));
+                    if($date2 > $date1) {
+                        $fail('Данный промокод не может   использоваться');
+                    }
+                },  ]],  [
             'name.required' => 'Введите промокод',
             'name.exists' => 'Промокод не найден'
         ]);
