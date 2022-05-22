d77b9443ad8bae223d34caee25fe7f8e2660f55a
Алексей <alexsynarchin@gmail.com>
2022-05-22 10:35:56 +0500

    Phone confirmation

--- app/Http/Controllers/Admin/Api/CityController.php
+++ app/Http/Controllers/Admin/Api/CityController.php
@@ -19,4 +19,42 @@ class CityController extends Controller
         return $cities;
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|unique:regions'
+        ], [
+            'title.required' => 'Введите название региона',
+            'title.unique' => 'Регион с таким названием уже существует'
+        ]);
+        $city = City::create($request->all());
+        return $city;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|unique:regions,title,' . $id
+        ], [
+            'title.required' => 'Введите название региона',
+            'title.unique' => 'Регион с таким названием уже существует'
+        ]);
+        $city = City::findOrFail($id);
+        $city = $city-> update($request->all());
+        return $city;
+    }
+
+    public function show($id)
+    {
+        $city = City::findOrFail($id);
+        return $city;
+    }
+
+    public function remove($id)
+    {
+        $city = City::findOrFail($id);
+        $city->delete();
+        return 'success';
+    }
+
 }
